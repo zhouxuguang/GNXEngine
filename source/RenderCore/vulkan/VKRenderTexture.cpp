a1bba55eb1a96bd56c59d85ad3f2df462e58dc95
zhouxuguang <zhouxuguang236@126.com>
2024-06-01 22:39:59 +0800

    vulkan ： 修复到渲染纹理格式设置的问题导致出来的图像只有红色通道的现象

--- source/RenderCore/vulkan/VKRenderTexture.cpp
+++ source/RenderCore/vulkan/VKRenderTexture.cpp
@@ -19,19 +19,19 @@ VKRenderTexture::VKRenderTexture(VulkanContextPtr context, const TextureDescript
     mFormat = format;
     assert(des.width > 0 && des.height > 0);
 
-    VkImageUsageFlags imageUsageFlags = VK_IMAGE_USAGE_TRANSFER_SRC_BIT |
-                                        VK_IMAGE_USAGE_TRANSFER_DST_BIT |
-                                        VK_IMAGE_USAGE_SAMPLED_BIT |
+    VkImageUsageFlags imageUsageFlags = VK_IMAGE_USAGE_SAMPLED_BIT |
                                         VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT |
-                                        VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT |
-                                        VK_IMAGE_USAGE_STORAGE_BIT | VK_IMAGE_USAGE_INPUT_ATTACHMENT_BIT;
+                                        VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT;
     
     VulkanBufferUtil::CreateImage2DVMA(mContext->vmaAllocator, des.width, des.height, format,
                                        VK_SAMPLE_COUNT_1_BIT, 1, VK_IMAGE_TILING_OPTIMAL, imageUsageFlags, mImage, mAllocation);
     
     //创建图像视图
-    VkImageView imageView = VulkanBufferUtil::CreateImageView(mContext->device, mImage, format, nullptr, 
-                            VK_IMAGE_ASPECT_COLOR_BIT | VK_IMAGE_ASPECT_DEPTH_BIT | VK_IMAGE_ASPECT_STENCIL_BIT, 1);
+    
+    // | VK_IMAGE_ASPECT_DEPTH_BIT | VK_IMAGE_ASPECT_STENCIL_BIT 这两个标记加上后图像就是红色的，也不知道为啥，
+    // 需要继续研究，应该需要判断缺失是深度模板的格式后再加上这些标记
+    VkImageView imageView = VulkanBufferUtil::CreateImageView(mContext->device, mImage, format, nullptr,
+                            VK_IMAGE_ASPECT_COLOR_BIT, 1);
     mVulkanImageViewPtr = std::make_shared<VulkanImageView>(mContext->device, imageView);
 }
 
@@ -46,6 +46,12 @@ VKRenderTexture::~VKRenderTexture()
     {
         vmaDestroyImage(mContext->vmaAllocator, mImage, mAllocation);
         mImage = VK_NULL_HANDLE;
+    }
+    
+    if (mAllocation)
+    {
+        vmaFreeMemory(mContext->vmaAllocator, mAllocation);
+        mAllocation = VK_NULL_HANDLE;
     }
 
     if (mVulkanImageViewPtr)
