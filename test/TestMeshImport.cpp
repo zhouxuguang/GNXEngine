a790786fc82f3bb0036bcf7b8ce827747d5d59da
zhouxuguang <zhouxuguang236@126.com>
2025-01-01 15:01:36 +0800

    添加新的测试mesh

--- test/TestMeshImport.cpp
+++ test/TestMeshImport.cpp
@@ -1,4 +1,6 @@
 #include "RenderSystem/mesh/MeshImporter.h"
+#include "RenderSystem/mesh/Mesh.h"
+#include "RenderSystem/RenderEngine.h"
 #include "pb.h"
 
 using namespace RenderSystem;
@@ -6,5 +8,12 @@ using namespace RenderSystem;
 int main(int argc, char* argv[])
 {
 	MeshImporter* meshImporter = CreateMeshImporter();
+	RenderSystem::MeshPtr mesh = std::make_shared<Mesh>();
+
+	std::string pathSplit = std::string(1, PATHSPLIT);
+	std::string filePath = "model" + pathSplit + "obj" + pathSplit + "box.obj";
+	std::string modelPath = getMediaDir() + filePath;
+
+	meshImporter->ImportFromFile(modelPath, mesh.get(), nullptr);
 	return 0;
 }
