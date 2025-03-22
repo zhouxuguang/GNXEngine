14e39a879729a1791cde68605ffd2099a3aced02
zhouxuguang <zhouxuguang236@126.com>
2025-03-22 12:29:15 +0800

    清理代码的中文乱码

--- include/BaseLib/AtomicOps.h
+++ include/BaseLib/AtomicOps.h
@@ -1,58 +1,58 @@
 #ifndef ATOMIC_OPS_INCLUDE_H
 #define ATOMIC_OPS_INCLUDE_H
 
-//原子操作封装
+//跨平台原子操作封装函数
 
 #include "PreCompile.h"
 
 NS_BASELIB_BEGIN
 
 /**
- *  ԭ�Ӳ����������ӣ��ڴ��ַ����4�ֽڶ���
+ *  原子操作函数，加，内存地址必须4字节对齐
  *
- *  @param ptrValue   ָ���ֵ
- *  @param nIncrement ���ӵ���
- *  @return ���ز���֮�����ֵ
+ *  @param ptrValue   指向的值
+ *  @param nIncrement 增加的数
+ *  @return 返回操作之后的新值
  */
 int BASELIB_API CBLAtomicAdd(volatile int* ptrValue, int nIncrement);
 
 /**
- *  ԭ�Ӳ�����������1����
- *  @param ptrValue   ָ���ֵ
- *  @return ���ز���֮�����ֵ
+ *  原子操作函数，加1操作
+ *  @param ptrValue   指向的值
+ *  @return 返回操作之后的新值
  */
 int BASELIB_API CBLAtomicIncrement(volatile int* ptrValue);
 
 /**
- *  ԭ�Ӳ�����������1����
- *  @param ptrValue   ָ���ֵ
- *  @return ���ز���֮�����ֵ
+ *  原子操作函数，减1操作
+ *  @param ptrValue   指向的值
+ *  @return 返回操作之后的新值
  */
 int BASELIB_API CBLAtomicDecrement(volatile int* ptrValue);
 
 /**
- *  ԭ��CAS���� �ڴ��ַ����4�ֽڶ���
- *  @param ptrValue  ָ���ֵ
- *  @param nExchange ������ֵ����ֵ
- *  @param nCompare  �Ƚϵ�ֵ����ֵ
- *  @return �����޸�֮ǰ��ֵ
+ *  原子CAS操作 内存地址必须4字节对齐
+ *  @param ptrValue  指向的值
+ *  @param nExchange 交换的值。新值
+ *  @param nCompare  比较的值，旧值
+ *  @return 返回修改之前的值
  */
 int BASELIB_API CBLAtomicCompareSwap(volatile int* ptrValue, int nExchange,int nCompare);
 
 /**
- *  ԭ��CAS����(����ָ��)
- *  @param theValue  ָ��Ķ���ָ��
- *  @param oldValue ������ֵ����ֵ
- *  @param newValue  �Ƚϵ�ֵ����ֵ
- *  @return �����Ƿ�ɹ�
+ *  原子CAS操作(操作指针)
+ *  @param theValue  指向的二级指针
+ *  @param oldValue 交换的值。旧值
+ *  @param newValue  比较的值，新值
+ *  @return 返回是否成功
  */
 bool BASELIB_API CBLAtomicCompareAndSwapPtr( void *oldValue, void *newValue, void * volatile *theValue );
 
 /**
- *  ԭ�ӽ���ָ�����(����ָ��)
+ *  原子交换指针操作(操作指针)
  *  @param oneValue
  *  @param otherValue
- *  @return �����Ƿ�ɹ�
+ *  @return 返回是否成功
  */
 bool BASELIB_API CBLAtomicSwapPtr( void *volatile *oneValue, void *volatile *otherValue );
 
