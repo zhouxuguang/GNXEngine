2aff2e3e4f9a8b06ff8aa606c785e0187a54deef
zhouxuguang <zhouxuguang236@126.com>
2025-03-23 22:13:22 +0800

    windows获得系统版本临时代码

--- source/BaseLib/EnvironmentUtility.cpp
+++ source/BaseLib/EnvironmentUtility.cpp
@@ -125,6 +125,126 @@ int EnvironmentUtility::GetProcessorCount() const
 
 #pragma warning(disable: 4996)
 
+#define INVALID_VERSION -1
+typedef enum _NTOS_VERSION {
+	NTOS_UNKNOWN,
+	NTOS_WINXP,
+	NTOS_WINXPSP1,
+	NTOS_WINXPSP2,
+	NTOS_WINXPSP3,
+	NTOS_WIN2003,
+	NTOS_WIN2003SP1,
+	NTOS_WIN2003SP2,
+	NTOS_WINVISTA,
+	NTOS_WINVISTASP1,
+	NTOS_WINVISTASP2,
+	NTOS_WIN7,
+	NTOS_WIN7SP1,
+	NTOS_WIN8,
+	NTOS_WIN81,
+	NTOS_WIN10_1507, //10240
+	NTOS_WIN10_1511, //10586
+	NTOS_WIN10_1607, //14393
+	NTOS_WIN10_1703, //15063
+	NTOS_WIN10_1709, //16299
+	NTOS_WIN10_1803, //17134
+	NTOS_WIN10_1809, //17763
+	NTOS_WIN10_1903, //18362
+	NTOS_WIN10_1909, //18363
+	NTOS_WIN10_2004, //19041
+	NTOS_WIN10_20H2, //19042
+	NTOS_WIN10_21H1, //19043
+	NTOS_WIN10_21H2, //19044
+	NTOS_WIN10_22H2, //19045
+	NTOS_WIN11_21H2, //22000
+	NTOS_WIN11_22H2, //22621
+	NTOS_WIN_MAX, //22621
+} NTOS_VERSION, * PNTOS_VERSION;
+
+typedef struct _OSVERSIONINFO
+{
+	DWORD dwOSVersionInfoSize;
+	DWORD dwMajorVersion;
+	DWORD dwMinorVersion;
+	DWORD dwBuildNumber;
+	DWORD dwPlatformId;
+	TCHAR szCSDVersion[128];
+} RtlOSVERSIONINFO;
+
+typedef NTSTATUS(WINAPI* RtlGetVersionPtr)(RtlOSVERSIONINFO*);
+
+NTOS_VERSION OsNtVersion()
+{
+	RtlOSVERSIONINFO info = { sizeof(RtlOSVERSIONINFO) };
+	HMODULE hNtdll = GetModuleHandle("ntdll.dll");
+	if (nullptr == hNtdll) 
+	{
+		return NTOS_UNKNOWN;
+	}
+
+	RtlGetVersionPtr RtlGetVersion = (RtlGetVersionPtr)GetProcAddress(hNtdll, "RtlGetVersion");
+	if (RtlGetVersion) 
+	{
+		RtlGetVersion(&info);
+		//printf("Version: %d.%d.%d\n", osvi.dwMajorVersion, osvi.dwMinorVersion, osvi.dwBuildNumber);
+	}
+
+	/*switch (info.dwMajorVersion) {
+	case 5: {
+		if (info.dwMinorVersion == 1) {
+			if (info.wServicePackMajor == 1) return NTOS_WINXPSP1;
+			if (info.wServicePackMajor == 2) return NTOS_WINXPSP2;
+			if (info.wServicePackMajor == 3) return NTOS_WINXPSP3;
+			return NTOS_WINXP;
+		}
+		if (info.dwMinorVersion == 2) {
+			if (info.wServicePackMajor == 1) return NTOS_WIN2003SP1;
+			if (info.wServicePackMajor == 2) return NTOS_WIN2003SP2;
+			return NTOS_WIN2003;
+		}
+		break;
+	} case 6: {
+		if (info.dwMinorVersion == 0) {
+			if (info.wServicePackMajor == 1) return NTOS_WINVISTASP1;
+			if (info.wServicePackMajor == 2) return NTOS_WINVISTASP2;
+			return NTOS_WINVISTA;
+		}
+		if (info.dwMinorVersion == 1) {
+			if (info.wServicePackMajor == 1) return NTOS_WIN7SP1;
+			return NTOS_WIN7;
+		}
+		if (info.dwMinorVersion == 2) {
+			return NTOS_WIN8;
+		}
+		if (info.dwMinorVersion == 3) {
+			return NTOS_WIN81;
+		}
+		break;
+	} case 10: {
+		if (info.dwBuildNumber == 10240) return NTOS_WIN10_1507;
+		if (info.dwBuildNumber == 10586) return NTOS_WIN10_1511;
+		if (info.dwBuildNumber == 14393) return NTOS_WIN10_1607;
+		if (info.dwBuildNumber == 15063) return NTOS_WIN10_1703;
+		if (info.dwBuildNumber == 16299) return NTOS_WIN10_1709;
+		if (info.dwBuildNumber == 17134) return NTOS_WIN10_1803;
+		if (info.dwBuildNumber == 17763) return NTOS_WIN10_1809;
+		if (info.dwBuildNumber == 18362) return NTOS_WIN10_1903;
+		if (info.dwBuildNumber == 18363) return NTOS_WIN10_1909;
+		if (info.dwBuildNumber == 19041) return NTOS_WIN10_2004;
+		if (info.dwBuildNumber == 19042) return NTOS_WIN10_20H2;
+		if (info.dwBuildNumber == 19043) return NTOS_WIN10_21H1;
+		if (info.dwBuildNumber == 19044) return NTOS_WIN10_21H2;
+		if (info.dwBuildNumber == 19045) return NTOS_WIN10_22H2;
+		if (info.dwBuildNumber == 22000) return NTOS_WIN11_21H2;
+		if (info.dwBuildNumber == 22621) return NTOS_WIN11_22H2;
+		if (info.dwBuildNumber > 22621) return NTOS_WIN_MAX;
+	}
+	default:
+		break;
+	}*/
+	return NTOS_UNKNOWN;
+}
+
 std::string EnvironmentUtility::GetSystemName() const
 {
 	OSVERSIONINFO vi;
@@ -145,6 +265,7 @@ std::string EnvironmentUtility::GetSystemName() const
 
 std::string EnvironmentUtility::GetDisplayName() const
 {
+	OsNtVersion();
 	OSVERSIONINFOEX vi;	// OSVERSIONINFOEX is supported starting at Windows 2000 
 	vi.dwOSVersionInfoSize = sizeof(vi);
 	if (GetVersionEx((OSVERSIONINFO*) &vi) == 0) return("Unknown");
