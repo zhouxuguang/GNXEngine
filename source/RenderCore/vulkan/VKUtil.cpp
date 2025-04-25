0592792fffaaa21dedd19ccd20905cd72384e35c
zhouxuguang <zhouxuguang236@126.com>
2025-04-25 23:08:52 +0800

    vulkan : vulkan fence池的封装，为后续资源异步加载做准备

--- source/RenderCore/vulkan/VKUtil.cpp
+++ source/RenderCore/vulkan/VKUtil.cpp
@@ -2,4 +2,38 @@
 
 NAMESPACE_RENDERCORE_BEGIN
 
+VulkanFencePtr VulkanFencePool::createFence(VkDevice device)
+{
+	if (mFreeFences.size() > 0)
+	{
+		VulkanFencePtr fence = mFreeFences.back();
+		mFreeFences.pop_back();
+		mBusyFences.push_back(fence);
+		return fence;
+	}
+
+	VulkanFencePtr newFence = std::make_shared<VulkanFence>();
+
+	VkFenceCreateInfo createInfo = {};
+	createInfo.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO;
+	newFence->init(device, createInfo);
+	mBusyFences.push_back(newFence);
+	return NULL;
+}
+
+void VulkanFencePool::releaseFence(VkDevice device, VulkanFencePtr fence)
+{
+	fence->reset(device);
+	for (uint32_t i = 0; i < mBusyFences.size(); ++i)
+	{
+		if (mBusyFences[i] == fence)
+		{
+			mBusyFences.erase(mBusyFences.begin() + i);
+			break;
+		}
+	}
+	mFreeFences.push_back(fence);
+	fence = nullptr;
+}
+
 NAMESPACE_RENDERCORE_END
--- source/RenderCore/vulkan/VKUtil.h
+++ source/RenderCore/vulkan/VKUtil.h
@@ -12,6 +12,8 @@
 
 NAMESPACE_RENDERCORE_BEGIN
 
+#define ASSERT assert
+
 // 将ptr连接到chainStart的next指针，ptr的next指向chainStart原先的pNext
 template <typename VulkanStruct1, typename VulkanStruct2>
 void AddToPNextChain(VulkanStruct1 *chainStart, VulkanStruct2 *ptr)
@@ -46,6 +48,110 @@ void AppendToPNextChain(VulkanStruct1 *chainStart, VulkanStruct2 *ptr)
     }
     endPtr->pNext = reinterpret_cast<VkBaseOutStructure *>(ptr);
 }
+
+// A helper class to disallow copy and assignment operators
+class NonCopyable
+{
+protected:
+	constexpr NonCopyable() = default;
+	~NonCopyable() = default;
+
+private:
+	NonCopyable(const NonCopyable&) = delete;
+	void operator=(const NonCopyable&) = delete;
+};
+
+// vulkan对象的包装类
+template <typename DerivedT, typename HandleT>
+class WrappedObject : NonCopyable
+{
+public:
+	HandleT getHandle() const { return mHandle; }
+	void setHandle(HandleT handle) { mHandle = handle; }
+	bool valid() const { return (mHandle != VK_NULL_HANDLE); }
+
+	const HandleT* ptr() const { return &mHandle; }
+
+	HandleT release()
+	{
+		HandleT handle = mHandle;
+		mHandle = VK_NULL_HANDLE;
+		return handle;
+	}
+
+protected:
+	WrappedObject() : mHandle(VK_NULL_HANDLE) {}
+	~WrappedObject() { ASSERT(!valid()); }
+
+	WrappedObject(WrappedObject&& other) : mHandle(other.mHandle)
+	{
+		other.mHandle = VK_NULL_HANDLE;
+	}
+
+	// Only works to initialize empty objects, since we don't have the device handle.
+	WrappedObject& operator=(WrappedObject&& other)
+	{
+		ASSERT(!valid());
+		std::swap(mHandle, other.mHandle);
+		return *this;
+	}
+
+	HandleT mHandle;
+};
+
+class VulkanFence final : public WrappedObject<VulkanFence, VkFence>
+{
+public:
+	VulkanFence() = default;
+	void destroy(VkDevice device)
+	{
+		if (valid())
+		{
+			vkDestroyFence(device, mHandle, nullptr);
+			mHandle = VK_NULL_HANDLE;
+		}
+	}
+	using WrappedObject::operator=;
+
+	VkResult init(VkDevice device, const VkFenceCreateInfo& createInfo)
+	{
+		ASSERT(!valid());
+		return vkCreateFence(device, &createInfo, nullptr, &mHandle);
+	}
+
+	VkResult reset(VkDevice device)
+	{
+		ASSERT(valid());
+		return vkResetFences(device, 1, &mHandle);
+	}
+
+	VkResult getStatus(VkDevice device) const
+	{
+		ASSERT(valid());
+		return vkGetFenceStatus(device, mHandle);
+	}
+
+	VkResult wait(VkDevice device, uint64_t timeout) const
+	{
+		ASSERT(valid());
+		return vkWaitForFences(device, 1, &mHandle, true, timeout);
+	}
+};
+
+using VulkanFencePtr = std::shared_ptr<VulkanFence>;
+
+// fence池,初始化时没有信号
+class VulkanFencePool
+{
+public:
+	VulkanFencePtr createFence(VkDevice device);
+
+	void releaseFence(VkDevice device, VulkanFencePtr fence);
+
+private:
+	std::vector<VulkanFencePtr> mFreeFences;
+	std::vector<VulkanFencePtr> mBusyFences;
+};
 
 NAMESPACE_RENDERCORE_END
 
