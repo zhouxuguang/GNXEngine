e3c30797cfaba907622cf77b586a215a2d92e38a
zhouxuguang <zhouxuguang236@126.com>
2025-08-10 18:29:18 +0800

    dxt1压缩的接口统一

--- source/AssetProcess/DXTCompressor.cpp
+++ source/AssetProcess/DXTCompressor.cpp
@@ -7,24 +7,30 @@
 
 NS_ASSETPROCESS_BEGIN
 
-void CompressDXT1(uint8_t* result, const uint8_t* input, uint32_t width, uint32_t height, uint32_t stride)
-{
-    goofy::compressDXT1(result, input, width, height, stride);
-}
-
+// DXT1数据压缩, input需要是rgba32的数据
 void CompressDXT1_ISPC(uint8_t* result, const uint8_t* input, uint32_t width, uint32_t height, uint32_t stride)
 {
-    rgba_surface srcData;
-    srcData.width = width;
-    srcData.height = height;
-    srcData.stride = stride;
-    srcData.ptr = (uint8_t*)input;
-    
-    rgba_surface fillboderData;
-    fill_borders(&fillboderData, &srcData, 4, 4);
-    
-    CompressBlocksBC1(&fillboderData, result);
-    free(fillboderData.ptr);
+	rgba_surface srcData;
+	srcData.width = width;
+	srcData.height = height;
+	srcData.stride = stride;
+	srcData.ptr = (uint8_t*)input;
+
+	rgba_surface fillboderData;
+	fill_borders(&fillboderData, &srcData, 4, 4);
+
+	CompressBlocksBC1(&fillboderData, result);
+	free(fillboderData.ptr);
+}
+
+void CompressDXT1(uint8_t* result, const uint8_t* input, uint32_t width, uint32_t height, uint32_t stride)
+{
+	if (width % 16 != 0 || height % 4 != 0)
+	{
+        return CompressDXT1_ISPC(result, input, width, height, stride);
+	}
+
+    goofy::compressDXT1(result, input, width, height, stride);
 }
 
 void CompressBC7(uint8_t* result, const uint8_t* input, uint32_t width, uint32_t height, uint32_t stride)
