46b2922c181d52e50ab18d10aa4314d16fe10ff2
zhouxuguang <zhouxuguang236@126.com>
2025-09-13 16:22:22 +0800

    全屏三角形顶点自动生成

--- source/shader/built-in/Atmosphere/ComputeTransmittance.hlsl
+++ source/shader/built-in/Atmosphere/ComputeTransmittance.hlsl
@@ -1,22 +1,23 @@
 #include "AtmosphereCommon.hlsl"
 
-// 输入结构体
-struct VS_INPUT
-{
-    float2 vertex : POSITION; // 使用语义绑定顶点位置
-};
-
 // 输出结构体
 struct VS_OUTPUT
 {
     float4 position : SV_POSITION; // 系统值语义表示顶点位置
 };
 
+float4 fsTrianglePosition(int vtx) 
+{
+  float x = -1.0 + float((vtx & 1) << 2);
+  float y = -1.0 + float((vtx & 2) << 1);
+  return float4(x, y, 0.0, 1.0);
+}
+
 [shader("vertex")]
-VS_OUTPUT VS(VS_INPUT input)
+VS_OUTPUT VS(uint vertexID : SV_VertexID)
 {
     VS_OUTPUT output;
-    output.position = float4(input.vertex, 0.0, 1.0);
+    output.position = fsTrianglePosition(vertexID);
     return output;
 }
 
