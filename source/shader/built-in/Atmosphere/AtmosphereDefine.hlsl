c2ed3323cbcf639d1378f8bee17a903ab87d56ee
zhouxuguang <zhouxuguang236@126.com>
2025-09-13 23:50:14 +0800

    显式指定结构体的偏移量，跨api可以放心使用constant buffer

--- source/shader/built-in/Atmosphere/AtmosphereDefine.hlsl
+++ source/shader/built-in/Atmosphere/AtmosphereDefine.hlsl
@@ -23,11 +23,11 @@
 //大气层密度剖面
 struct DensityProfileLayer 
 {
-	float width;
-	float exp_term;
-	float exp_scale;
-	float linear_term;
-	float constant_term;
+	[[vk::offset(0)]] float width;
+	[[vk::offset(4)]] float exp_term;
+	[[vk::offset(8)]] float exp_scale;
+	[[vk::offset(12)]] float linear_term;
+	[[vk::offset(16)]] float constant_term;
 };
 
 struct DensityProfile 
@@ -38,34 +38,41 @@ struct DensityProfile
 //大气层参数模型
 struct AtmosphereParameters
 {
+	// 太阳相关参数
 	// 大气层顶部的太阳辐照度
-	float3 solar_irradiance;
+	[[vk::offset(0)]] float3 solar_irradiance;
 	// 太阳角半径
-  	float sun_angular_radius;
+  	[[vk::offset(12)]] float sun_angular_radius;
+
+	// 星球相关参数
   	// 星球中心到大气层底部的距离, 即地球半径
-  	float bottom_radius;
+  	[[vk::offset(16)]] float bottom_radius;
   	// 星球中心到大气层顶部的距离
-  	float top_radius;
+  	[[vk::offset(20)]] float top_radius;
+
+	// Rayleigh散射系数
   	// 空气分子的密度分布,[0,1]
-  	DensityProfile rayleigh_density;
+  	[[vk::offset(32)]] DensityProfile rayleigh_density;
   	// 海拔为h处的rayleigh散射系数 = rayleigh_scattering * rayleigh_density
-  	float3 rayleigh_scattering;
+  	[[vk::offset(96)]] float3 rayleigh_scattering;
+
+	// Mie散射
   	// 气溶胶的密度剖面,[0,1]
-  	DensityProfile mie_density;
+  	[[vk::offset(112)]] DensityProfile mie_density;
 	// mie散射系数 = mie_scattering * mie_density
-  	float3 mie_scattering;
+  	[[vk::offset(176)]] float3 mie_scattering;
   	// 气溶胶的消光系数 = mie_extinction * mie_density
-  	float3 mie_extinction;
+  	[[vk::offset(192)]] float3 mie_extinction;
   	// 气溶胶的Cornette-Shanks相位函数中的非对称参数
-  	float mie_phase_function_g;
+  	[[vk::offset(204)]] float mie_phase_function_g;
   	// 吸收光线的空气分子的密度分布,[0,1]
-  	DensityProfile absorption_density;
+  	[[vk::offset(208)]] DensityProfile absorption_density;
   	// 吸收光线的空气消光系数 = absorption_extinction * absorption_density
-  	float3 absorption_extinction;
+  	[[vk::offset(272)]] float3 absorption_extinction;
   	// 地面的平均反照率(反射系数)
-  	float3 ground_albedo;
+  	[[vk::offset(288)]] float3 ground_albedo;
   	// 太阳天最大天顶角的cos值(cos因此最小)，用于后面大气散射的预计算
-  	float mu_s_min;
+  	[[vk::offset(300)]] float mu_s_min;
 };
 
 #endif
