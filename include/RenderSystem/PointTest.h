e94c5ebeea9fa20ecedc9490b797de73c58dd897
zhouxuguang <zhouxuguang236@126.com>
2025-10-07 23:34:55 +0800

    几何：整理相交的代码

--- include/RenderSystem/PointTest.h
+++ include/RenderSystem/PointTest.h
@@ -45,16 +45,75 @@ public:
     }
 
     //判断点是否在AABB中
-    //static bool PointInAABB(const Vector3f& point, const AABB& aabb);
+    template<typename T>
+    static bool PointInAABB(const Vector3<T>& point, const AxisAlignedBox<T>& aabb)
+    {
+        const Vector3<T>& min = aabb.minimum;
+        const Vector3<T>& max = aabb.maximum;
+
+        if (point.x < min.x || point.y < min.y || point.z < min.z)
+        {
+            return false;
+        }
+        if (point.x > max.x || point.y > max.y || point.z > max.z)
+        {
+            return false;
+        }
+
+        return true;
+    }
 
     //计算点离AABB最近的点
-    //static Vector3f ClosestPoint(const AABB& aabb, const Vector3f& point);
+    template<typename T>
+    static Vector3<T> ClosestPoint(const AxisAlignedBox<T>& aabb, const Vector3<T>& point)
+    {
+        Vector3<T> result = point;
+        const Vector3<T>& min = aabb.minimum;
+        const Vector3<T>& max = aabb.maximum;
+
+        result.x = (result.x < min.x) ? min.x : result.x;
+        result.y = (result.y < min.x) ? min.y : result.y;
+        result.z = (result.z < min.x) ? min.z : result.z;
+
+        result.x = (result.x > max.x) ? max.x : result.x;
+        result.y = (result.y > max.x) ? max.y : result.y;
+        result.z = (result.z > max.x) ? max.z : result.z;
+
+        return result;
+    }
 
     //判断点是否在OBB中
-    //static bool PointInOBB(const Vector3f& point, const OBB& obb);
+    template<typename T>
+    static bool PointInOBB(const Vector3<T>& point, const OrientedBoundingBox<T>& obb)
+    {
+//        Vector3f dir = point - obb.mCenter;
+//
+//        for (int i = 0; i < 3; ++i)
+//        {
+//            const float* orientation = &obb.orientation.asArray[i * 3];
+//            vec3 axis(orientation[0], orientation[1], orientation[2]);
+//
+//            float distance = Dot(dir, axis);
+//
+//            if (distance > obb.size.asArray[i])
+//            {
+//                return false;
+//            }
+//            if (distance < -obb.size.asArray[i])
+//            {
+//                return false;
+//            }
+//        }
+
+        return true;
+    }
 
 	//计算点离OBB最近的点
-	//static Vector3f ClosestPoint(const OBB& obb, const Vector3f& point);
+    template<typename T>
+	static Vector3<T> ClosestPoint(const OrientedBoundingBox<T>& obb, const Vector3<T>& point)
+    {
+        return Vector3<T>();
+    }
 };
 
 
