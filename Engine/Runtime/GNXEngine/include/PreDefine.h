aa0b8216b0cb26ab3948fba2ca942c7eb914f7c2
zhouxuguang <zhouxuguang236@126.com>
2025-10-25 16:51:00 +0800

    engine

---
+++ Engine/Runtime/GNXEngine/include/PreDefine.h
@@ -0,0 +1,42 @@
+/**
+ * 游戏引擎预定义头文件
+ */
+
+#ifndef GNX_ENGINE_PREDEFINE_JDGJDFJVGNDFKN_INCLUDE
+#define GNX_ENGINE_PREDEFINE_JDGJDFJVGNDFKN_INCLUDE
+
+#define NAMESPACE_GNXENGINE_BEGIN        namespace GNXEngine {
+/** namespace结束宏 */
+#define NAMESPACE_GNXENGINE_END            }
+
+#define USING_NS_GNXENGINE                     using namespace GNXEngine;
+
+#include "BaseLib/BaseLib.h"
+
+//导出宏定义
+#if defined _WIN32 || defined __CYGWIN__ || _WIN64
+	#ifdef GNXENGINE_EXPORTS		
+		#ifdef __GNUC__
+			#define GNXENGINE_API __attribute__((dllexport))
+		#else
+			#define GNXENGINE_API __declspec(dllexport)
+		#endif
+	#else
+		#ifdef __GNUC__
+			#define GNXENGINE_API __attribute__((dllimport))
+		#else
+			#define GNXENGINE_API __declspec(dllimport)
+		#endif
+	#endif
+	#define GNXENGINE_API_HIDE
+#else
+	#if __GNUC__>=4
+		#define GNXENGINE_API __attribute__((visibility("default")))
+		#define GNXENGINE_API_HIDE __attribute__ ((visibility("hidden")))
+	#else
+		#define GNXENGINE_API
+		#define GNXENGINE_API_HIDE
+	#endif
+#endif
+
+#endif
---
+++ Engine/Runtime/GNXEngine/source/GameEngine.cpp
@@ -0,0 +1,27 @@
+#include "GameEngine.h"
+
+NAMESPACE_GNXENGINE_BEGIN
+
+GameEngine::GameEngine(/* args */)
+{
+}
+
+GameEngine::~GameEngine()
+{
+}
+
+bool GameEngine::Initlize()
+{
+	return false;
+}
+
+void GameEngine::Unload()
+{
+}
+
+bool GameEngine::Tick()
+{
+	return false;
+}
+
+NAMESPACE_GNXENGINE_END
