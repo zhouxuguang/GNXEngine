db376be32943d20ba20ea71201e4b232f1fe3e6a
zhouxuguang <zhouxuguang236@126.com>
2025-12-28 17:16:10 +0800

    rdg : 完善passnode

--- Engine/Runtime/GNXEngine/include/FrameGraph/PassNode.h
+++ Engine/Runtime/GNXEngine/include/FrameGraph/PassNode.h
@@ -72,12 +72,82 @@ public:
 
     void registerResource(FrameGraphHandle resourceHandle) noexcept;
 
-    virtual void execute(/* FrameGraphResources const& resources, backend::DriverApi& driver*/) noexcept = 0;
+    virtual void execute(/*FrameGraphResources const& resources, backend::DriverApi& driver*/) noexcept = 0;
     virtual void resolve() noexcept = 0;
     std::string graphvizifyEdgeColor() const noexcept override;
 
     //Vector<VirtualResource*> devirtualize;         // resources we need to create before executing
     //Vector<VirtualResource*> destroy;              // resources we need to destroy after executing
+};
+
+#if 0
+
+class RenderPassNode : public PassNode 
+{
+public:
+    class RenderPassData 
+    {
+    public:
+        static constexpr size_t ATTACHMENT_COUNT = 8 + 2;
+        std::string name{};
+        FrameGraphRenderPass::Descriptor descriptor;
+        bool imported = false;
+        backend::TargetBufferFlags targetBufferFlags = {};
+        FrameGraphId<FrameGraphTexture> attachmentInfo[ATTACHMENT_COUNT] = {};
+        ResourceNode* incoming[ATTACHMENT_COUNT] = {};  // nodes of the incoming attachments
+        ResourceNode* outgoing[ATTACHMENT_COUNT] = {};  // nodes of the outgoing attachments
+        struct 
+        {
+            backend::Handle<backend::HwRenderTarget> target;
+            backend::RenderPassParams params;
+        } backend;
+
+        void devirtualize(FrameGraph& fg, TextureCacheInterface& textureCache) noexcept;
+        void destroy(TextureCacheInterface& textureCache) const noexcept;
+    };
+
+    RenderPassNode(FrameGraph& fg, const char* name, FrameGraphPassBase* base) noexcept;
+    RenderPassNode(RenderPassNode&& rhs) noexcept;
+    ~RenderPassNode() noexcept override;
+
+    uint32_t declareRenderTarget(FrameGraph& fg, FrameGraph::Builder& builder,
+            utils::StaticString name, FrameGraphRenderPass::Descriptor const& descriptor);
+
+    RenderPassData const* getRenderPassData(uint32_t id) const noexcept;
+
+private:
+    // virtuals from DependencyGraph::Node
+    char const* getName() const noexcept override { return mName; }
+    utils::CString graphvizify() const noexcept override;
+    void execute(FrameGraphResources const& resources, backend::DriverApi& driver) noexcept override;
+    void resolve() noexcept override;
+
+    // constants
+    const char* const mName = nullptr;
+    UniquePtr<FrameGraphPassBase, LinearAllocatorArena> mPassBase;
+
+    // set during setup
+    std::vector<RenderPassData> mRenderTargetData;
+};
+
+#endif
+
+
+// 最终用于上屏的渲染节点
+class PresentPassNode : public PassNode
+{
+public:
+    explicit PresentPassNode(FrameGraph& fg) noexcept;
+    PresentPassNode(PresentPassNode&& rhs) noexcept;
+    ~PresentPassNode() noexcept override;
+    PresentPassNode(PresentPassNode const&) = delete;
+    PresentPassNode& operator=(PresentPassNode const&) = delete;
+    void execute(/*FrameGraphResources const& resources, backend::DriverApi& driver*/) noexcept override;
+    void resolve() noexcept override;
+private:
+    // virtuals from DependencyGraph::Node
+    char const* getName() const noexcept override;
+    std::string graphvizify() const noexcept override;
 };
 
 NAMESPACE_GNXENGINE_END
