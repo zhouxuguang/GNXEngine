408847f038d892b6867a4f73e4f43acfa8ef3b7c
zhouxuguang <zhouxuguang236@126.com>
2026-05-20 22:15:06 +0800

    虚拟纹理： 完善LRU缓存的管理

--- Engine/Runtime/RenderSystem/source/VirtualTexture/VirtualTextureCache.cpp
+++ Engine/Runtime/RenderSystem/source/VirtualTexture/VirtualTextureCache.cpp
@@ -40,6 +40,11 @@ CacheAllocation VirtualTextureCache::Allocate(const PageRequest& request)
 
     // LRU 淘汰
     PageRequest evicted = FindEvictionCandidate();
+    if (mActiveAllocations.find(evicted) == mActiveAllocations.end())
+    {
+        // 所有 page 都被 pinned，没有可淘汰的
+        return {false, {}, false, {}};
+    }
     Evict(evicted);
     PageSlot slot = mActiveAllocations[evicted];
     mActiveAllocations.erase(evicted);
@@ -50,15 +55,29 @@ CacheAllocation VirtualTextureCache::Allocate(const PageRequest& request)
 
 void VirtualTextureCache::Touch(const PageRequest& request)
 {
-    // TODO: 将 request 移到 mLRUList 头部。
+    if (request.mipLevel >= mPinnedMipLevels)
+    {
+        return; // no-op for pinned lods
+    }
+
+	if (auto it = mLRUMap.find(request); it != mLRUMap.end())
+    {
+        mLRUList.splice(mLRUList.begin(), mLRUList, it->second);
+	}
 }
 
 void VirtualTextureCache::Commit(const PageRequest& request, const PageSlot& slot)
 {
+    // Allocate() 已确保 request 未被分配，直接写入映射并加入 LRU
     mActiveAllocations[request] = slot;
     mSlotOwners[slot] = request;
-    mLRUList.push_front(request);
-    mLRUMap[request] = mLRUList.begin();
+
+    // pinned page 不会被淘汰，不需要加入 LRU
+    if (request.mipLevel < mPinnedMipLevels)
+    {
+        mLRUList.push_front(request);
+        mLRUMap[request] = mLRUList.begin();
+    }
 }
 
 void VirtualTextureCache::FreeSlot(const PageSlot& slot)
@@ -68,14 +87,28 @@ void VirtualTextureCache::FreeSlot(const PageSlot& slot)
 
 PageRequest VirtualTextureCache::FindEvictionCandidate() const
 {
-    // TODO: 从 mLRUList 末尾查找第一个 mipLevel >= mPinnedMipLevels 的 page 返回。
-    return { 0, 0, 0 };
+    // 从 LRU 尾部（最久未使用）向前遍历，找到第一个非 pinned 的 page
+    for (auto it = mLRUList.rbegin(); it != mLRUList.rend(); ++it)
+    {
+        if (it->mipLevel < mPinnedMipLevels)
+        {
+            return *it;
+        }
+    }
+    // 所有 page 都被 pinned，调用方 Allocate 通过 mActiveAllocations 检查兜底
+    return {0, 0, 0};
 }
 
 void VirtualTextureCache::Evict(const PageRequest& request)
 {
-    // TODO: 清除 page table 中的 resident 标记。
-    // TODO: 从 LRU 列表中移除。
+    // 从 LRU 索引表中找到链表节点迭代器，删除节点并清除索引
+    auto it = mLRUMap.find(request);
+    if (it != mLRUMap.end())
+    {
+        mLRUList.erase(it->second);
+        mLRUMap.erase(it);
+    }
+    // mActiveAllocations 和 mSlotOwners 由调用方 Allocate 自行清理
 }
 
 NS_RENDERSYSTEM_END
