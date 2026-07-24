58eca80a1145908b04e16406459f630fc0b5596d
zhouxuguang <zhouxuguang236@126.com>
2026-07-24 22:10:00 +0800

    metis  : 修复Mac系统构建失败的问题

--- ThirdParty/metis/metis/GKlib/GKlibSystem.cmake
+++ ThirdParty/metis/metis/GKlib/GKlibSystem.cmake
@@ -109,7 +109,7 @@ else()
   if("${HAVE_THREADLOCALSTORAGE}" MATCHES "^${HAVE_THREADLOCALSTORAGE}$")
     try_compile(HAVE_THREADLOCALSTORAGE
       ${CMAKE_BINARY_DIR}
-      ${CMAKE_SOURCE_DIR}/${GKLIB_PATH}/conf/check_thread_storage.c)
+      ${METIS_PATH}/${GKLIB_PATH}/conf/check_thread_storage.c)
     if(HAVE_THREADLOCALSTORAGE)
       message(STATUS "checking for thread-local storage - found")
     else()
